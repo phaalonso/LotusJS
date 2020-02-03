d4df87fb2120c7c78c9bbc625db6ff8a48af14ee
Pedro Henrique de Almeida Alonso ALMEIDA ALONSO <phaalonso@gmail.com>
2020-02-03 19:47:00 -0300

    Warframe Analyze working

--- commands/warframe-analyze.js
+++ commands/warframe-analyze.js
@@ -1,5 +1,9 @@
-const { WarframeMarket } = require('./services/api');
+const { WarframeMarket } = require('./services/WarframeMarketApi');
+const axios = require('axios');
+
 const filter_order = require('./utils/filter_order');
+
+const baseURL = 'https://api.warframe.market/v1';
 
 module.exports = {
     name: 'analyze',
@@ -7,54 +11,51 @@ module.exports = {
     args: true,
     usage: '<item>',
     async execute(message, args) {
-        if (args[args.length -1] === 'set'){
+        if (args[args.length - 1] === 'set') {
             const setInfo = await WarframeMarket.get(`/items/${args.join('_')}`);
             // console.log(setInfo);
             if (setInfo.status = 200) {
                 /*
                     Getting the items of the set!
                 */
-                const itemSet = [] 
+                const itemList = []
                 setInfo.data.payload.item.items_in_set.map((item) => {
-                    if ( !item.de.item_name.endsWith('Set') ) {
-                        itemSet.push(
+                    if (!item.de.item_name.endsWith('Set')) {
+                        itemList.push(
                             item.de.item_name.toLowerCase()
                                 .split(/ +/)
                                 .join('_')
-                        );   
+                        );
                     }
                 });
                 /*
                     Getting the analyzed price
                 */
-                console.log(itemSet);
+                console.log('Item list', itemList);
                 let price = 0;
-                for(const item of itemSet) {
-                    const response = await WarframeMarket.get(`/items/${item}/orders`);
-                    if ( response.status = 200 ) {
-                        const { orders } = response.data.payload;
-                        const filter = filter_order(orders);
-                        // console.log(item ,filter[0], filter[0].platinum);
-                        price += filter[0].platinum;
-                        console.log(item, price);
-                    } else {
-                        console.error('Erro in getting the order of the item: ', item);
+                const links = [];
+                itemList.map(item => links.push(axios.get(`${baseURL}/items/${item}/orders`)));
+                const responses = await axios.all(links)
+
+                for (const rep of responses) {
+                    if ( rep.status = 200 ) {
+                        price += filter_order(rep.data.payload.orders)[0].platinum;
                     }
                 }
 
                 let setPrice = -1;
-                const response = await WarframeMarket.get(`/items/${args.join('_')}/orders?include=statistics`);
+                const response = await WarframeMarket.get(`/items/${args.join('_')}/orders`);
                 if ( response.status = 200 ) {
                     const { orders } = response.data.payload;
                     const filter = filter_order(orders);
                     setPrice = filter[0].platinum;
                 }
 
-                return message.channel.send(`The price analyzed is: ${price}, while the actual set price is: ${setPrice}`);
+                return message.channel.send(`The price of the buying the pieces separately is: ${price}, while the actual set price is: ${setPrice}`);
             } else {
                 message.channel.send('Sorry we have a error!');
             }
-        } else { 
+        } else {
             return message.channel.send('Sorry this only work for set!');
         }
     },
